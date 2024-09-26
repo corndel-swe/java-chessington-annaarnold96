94f9baa5f684c6d93faab596e8d33be98a0f7630
Anna Arnold <anna.arnold.96@gmail.com>
2024-09-26 13:54:52 +0100

    Fixes Rook moves vertically

--- src/main/java/com/corndel/chessington/model/pieces/Rook.java
+++ src/main/java/com/corndel/chessington/model/pieces/Rook.java
@@ -24,27 +24,26 @@ public class Rook extends AbstractPiece implements Piece {
     // For every row, check if there is anything. If null, add move. If false, break.
 
     // Move down
-    if (!board.wouldMoveOffBoardVertically(from, board)) {
+//    if (!board.wouldMoveOffBoardVertically(from, board)) {
       int spacesCanMoveDown = 1;
       for (int row = from.getRow() + 1; row <= 7; row++) {
-        if (board.get(from.plus(row, 0)) == null) {
+        if (board.get(from.plus(spacesCanMoveDown, 0)) == null) {
           allowedMoves.add(new Move(from, from.plus(spacesCanMoveDown, 0)));
           spacesCanMoveDown++;
         } else break;
       }
-    }
+//    }
 
       // Move up
-    if (!board.wouldMoveOffBoardVertically(from, board)){
+//    if (!board.wouldMoveOffBoardVertically(from, board)){
       int spacesCanMoveUp = -1;
       for (int row = from.getRow() - 1; row >= 0; row--) {
-        if (board.get(from.plus(row, 0)) == null) {
+        if (board.get(from.plus(spacesCanMoveUp, 0)) == null) {
           allowedMoves.add(new Move(from, from.plus(spacesCanMoveUp, 0)));
-          System.out.println(allowedMoves);
           spacesCanMoveUp--;
         } else break;
       }
-    }
+//    }
 
 
 
