275437f658b397ee1b2bcbd57f3f90a560886cf3
Anna Arnold <anna.arnold.96@gmail.com>
2024-09-26 14:23:45 +0100

    fixes rook and passes all tests

--- src/main/java/com/corndel/chessington/model/pieces/Rook.java
+++ src/main/java/com/corndel/chessington/model/pieces/Rook.java
@@ -24,26 +24,65 @@ public class Rook extends AbstractPiece implements Piece {
     // For every row, check if there is anything. If null, add move. If false, break.
 
     // Move down
-//    if (!board.wouldMoveOffBoardVertically(from, board)) {
       int spacesCanMoveDown = 1;
       for (int row = from.getRow() + 1; row <= 7; row++) {
         if (board.get(from.plus(spacesCanMoveDown, 0)) == null) {
           allowedMoves.add(new Move(from, from.plus(spacesCanMoveDown, 0)));
           spacesCanMoveDown++;
-        } else break;
+        }
+        else if ((board.get(from.plus(spacesCanMoveDown, 0)) != null) && (board.get(from.plus(spacesCanMoveDown, 0)).getColour() != getColour())){
+          allowedMoves.add(new Move(from, from.plus(spacesCanMoveDown, 0)));
+          break;
+        }
+        else break;
       }
-//    }
+
 
       // Move up
-//    if (!board.wouldMoveOffBoardVertically(from, board)){
+
       int spacesCanMoveUp = -1;
       for (int row = from.getRow() - 1; row >= 0; row--) {
         if (board.get(from.plus(spacesCanMoveUp, 0)) == null) {
           allowedMoves.add(new Move(from, from.plus(spacesCanMoveUp, 0)));
           spacesCanMoveUp--;
-        } else break;
+        }
+        else if ((board.get(from.plus(spacesCanMoveUp, 0)) != null) && (board.get(from.plus(spacesCanMoveUp, 0)).getColour() != getColour())){
+          allowedMoves.add(new Move(from, from.plus(spacesCanMoveUp, 0)));
+          break;
+        }
+        else break;
       }
-//    }
+
+    // Move Right
+    int spacesCanMoveRight = 1;
+    for (int col = from.getCol() + 1; col <= 7; col++) {
+      if (board.get(from.plus(0, spacesCanMoveRight)) == null) {
+        allowedMoves.add(new Move(from, from.plus(0, spacesCanMoveRight)));
+        spacesCanMoveRight++;
+      } else if ((board.get(from.plus(0, spacesCanMoveRight)) != null) && (board.get(from.plus(0, spacesCanMoveRight)).getColour() != getColour())){
+        allowedMoves.add(new Move(from, from.plus(0, spacesCanMoveRight)));
+        break;
+      }
+
+      else break;
+    }
+
+
+    // Move Left
+
+    int spacesCanMoveLeft = -1;
+    for (int col = from.getCol() - 1; col >= 0; col--) {
+      if (board.get(from.plus(0, spacesCanMoveLeft)) == null) {
+        allowedMoves.add(new Move(from, from.plus(0, spacesCanMoveLeft)));
+        spacesCanMoveLeft--;
+      }
+      else if ((board.get(from.plus(0, spacesCanMoveLeft)) != null) && (board.get(from.plus(0, spacesCanMoveLeft)).getColour() != getColour())){
+        allowedMoves.add(new Move(from, from.plus(0, spacesCanMoveLeft)));
+        break;
+      }
+      else break;
+    }
+
 
 
 
